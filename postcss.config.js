69eda93dd1832419fe0a5631701631bf5733224f
Aman Khan <amankhancompq2002@gmail.com>
2024-01-28 14:24:16 +0530

    added Toolbox and Menu bar

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {}, // Import all the css files into one master file
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Menu from "@/components/Menu";
+import Toolbox from "@/components/Toolbox";
+export default function Home() {
+  return (
+    <>
+      <Menu/>
+      <Toolbox/>
+    </>
+  );
+}
